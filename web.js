e118cad46a4275584b66d379850f1c170e264a33
Ubuntu <ubuntu@ip-172-31-30-48.us-west-2.compute.internal>
2013-07-14 01:53:39 +0000

    using index.html for response

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var stringResponse = getIndexResponse();
+  response.send(stringResponse);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var getHardCodedResponse = function() {
+  return "Hello World 2!";
+}
+
+var getIndexResponse = function() {
+  return fs.readFileSync("index.html").toString();
+}
